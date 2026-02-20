e30bb03772cd8f395a3b35ad4378e7d60204b2d6
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-02-21 02:29:45 +0600

    completed the login page

---
+++ script/login.js
@@ -0,0 +1,31 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+    // step  1 : get the mobile number
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    // console.log(contactNumber)
+
+
+    // 2: get the pin input
+    const pinInput = document.getElementById("input-pin");
+    const pin = pinInput.value;
+    // console.log(pin);
+
+
+    // 3: Match pin & mobile number
+    if (contactNumber == "01234567890" && pin == "1234") {
+        // 3-1 true::: alert> homepage
+        alert("login Successful");
+
+
+        //replace() নতুন page এ নিয়ে যায় and browser history theke current page টা remove kore দেয় .user Back button চাপলে আগের page এ ফিরে আসতে পারে না
+        //assign() নতুন page এ নিয়ে যায় but browser history তে current page টা রেখে দেয় .user Back button চাপলে আগের page এ ফিরে আসতে পারে
+        
+        // window.location.replace("/home.html")
+        window.location.assign("/home.html")
+
+    } else {
+        // 3-1 false::: alert> return
+        alert("logIn failed");
+        return;
+    }
+})
